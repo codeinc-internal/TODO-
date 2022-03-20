08bd6b608f8c69e504aee2593621dd01322a738b
harris-sudo <70772405+harris-sudo@users.noreply.github.com>
2022-03-21 00:02:55 +0500

    first commit

---
+++ build/precache-manifest.d649debec1c299bd8bfe1ab32bfacbc7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54bc8d4e9c3073bc76b2fc06d12f0c7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a359d6f2db00b2f43600",
+    "url": "/static/js/2.306c8396.chunk.js"
+  },
+  {
+    "revision": "2708bb4066eb0640b4cb",
+    "url": "/static/js/main.d6b7492d.chunk.js"
+  },
+  {
+    "revision": "e15cf135dc78fdc2ad46",
+    "url": "/static/js/runtime-main.d3a0f9ba.js"
+  }
+]);
